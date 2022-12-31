06ebbf3e46717fcd154eb12bbd5d8d51e7fdd605
tde-nico <101901057+tde-nico@users.noreply.github.com>
2022-12-31 09:51:00 +0100

    textured_cube

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class GraphicEngine: # 2 50
 		pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
 		pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
 		self.ctx = mgl.create_context()
+		self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
 		self.clock = pg.time.Clock()
 		self.time = 0
 		# Scene
